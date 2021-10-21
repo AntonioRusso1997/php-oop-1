f9559c7f8e828c08af9522d2e2e41669ba898499
Antonio <antonioru1997@gmail.com>
2021-10-21 12:03:45 +0200

    Classe Movie creata

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
+    class Movie {
+        public $title;
+        public $genre;
+        public $director;
+        public $country;
+        public $year;
+        public $mainActors;
+        public $vote;
+    }
 
 ?>
