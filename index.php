a795085abd9a090dd49fe4933370abeb9cc5ee70
Antonio <antonioru1997@gmail.com>
2021-10-21 15:27:06 +0200

    FINE con mini-bonus

--- index.php
+++ index.php
@@ -34,25 +34,37 @@
         function setVote($_vote) {
             $this->vote = $_vote;
         }
+
+        function getInfo() {
+            return 
+                '<h2> Titolo: ' .$this->title .'</h2>'.
+                '<h2> Genere: ' .$this->title .'</h2>'.
+                '<h2> Regista: ' .$this->director .'</h2>'.
+                '<h2> Sceneggiatura: ' .$this->screenplay .'</h2>'.
+                '<h2> Paese: ' .$this->country .'</h2>'.
+                '<h2> Anno di uscita: ' .$this->year .'</h2>'.
+                '<h2> Cast Principale: ' .$this->mainCast .'</h2>'.
+                '<h2> Voto: ' .$this->vote .'</h2>';
+        }
     }
 
     $firstMovie = new Movie('Avengers - Endgame', 'Avventura, Fantascienza, Azione', 'Anthony Russo, Joe Russo', 'Stephen McFeely', 'United States', 2019, 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hermsworth', 9.2);    
     $secondMovie = new Movie('Natale in India', 'Commedia', 'Neri Parenti', 'Lorenzo De Luca', 'Italy', 2003, 'Christian De Sica, Massimo Boldi, Enzo Salvi, Biagio Izzo', 4.1);
 
     //Stampo primo film
-    echo '<h2>Primo Film: ' .$firstMovie->getTitle() .'</h2>';
+    echo '<h1 style="color: red">Primo Film: ' .$firstMovie->getTitle() .'</h1>';
     var_dump($firstMovie);
-    echo '<h3>Voto: ' .$firstMovie->getVote()  .'</h3>';
+    echo $firstMovie->getInfo();
     $firstMovie->setVote(7.2); // 'Setto' un nuovo voto
-    echo '<h3>Voto (con setVote): ' .$firstMovie->getVote()  .'</h3>';
+    echo '<h2 style="color:blue">Voto (con setVote): ' .$firstMovie->getVote()  .'</h2>';
 
     echo '<hr>';
     
     //Stampo secondo film
-    echo '<h2>Secondo Film: ' .$secondMovie->getTitle() .'</h2>';
+    echo '<h1 style="color: red">Secondo Film: ' .$secondMovie->getTitle() .'</h1>';
     var_dump($secondMovie);
-    echo '<h3>Voto: ' .$secondMovie->getVote()  .'</h3>';
+    echo $secondMovie->getInfo();
     $secondMovie->setVote(10); // 'Setto' un nuovo voto
-    echo '<h3>Voto (con setVote): ' .$secondMovie->getVote()  .'</h3>';
+    echo '<h2 style="color:blue">Voto (con setVote): ' .$secondMovie->getVote()  .'</h2>';
 
 ?>
